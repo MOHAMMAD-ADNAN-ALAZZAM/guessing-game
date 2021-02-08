584afa278ad361353ebfcc751e33723f7e5c9311
MOHAMMAD ADNAN AL AZZAM <mohammadalazzam730@gmail.com>
2021-02-08 17:13:13 +0200

    red

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+
+let userName = prompt('Your Name ?');
+console.log(userName);
+alert('wellcome'+ userName);
+
+
+let q1 = prompt('Do you like physics?');
+console.log(q1);
+if(q1.toLocaleUpperCase === 'yes' || q1 === 'y'){
+    alert('You are look like me');
+}else if('q1 ===no'|| 'q1 === n'){
+alert('You are not like me');
+}else{
+    alert('wellcom'+userName);
+}
+
+let q1 = prompt('Do you like sports ?');
+
+
+let q1 = prompt('Do you like fighting games?');
+
+
+let q1 = prompt('do you like drawing ?');
+
+
+let q1 = prompt('Do you want to be a programmer?');
+
+
+
+alert('Nice to meet you'+ userName);
+
+
+
+
+
