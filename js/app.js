2447f22abc83e7b8011846ac22ee02a586a66d3c
MOHAMMAD ADNAN AL AZZAM <mohammadalazzam730@gmail.com>
2021-02-08 18:39:41 +0200

    red

--- js/app.js
+++ js/app.js
@@ -1,36 +1,61 @@
-
+'use strict';
 
 let userName = prompt('Your Name ?');
 console.log(userName);
-alert('wellcome'+ userName);
+alert('wellcome '+ userName);
+
 
 
 let q1 = prompt('Do you like physics?');
 console.log(q1);
-if(q1.toLocaleUpperCase === 'yes' || q1 === 'y'){
+if(q1.toLowerCase() === 'yes' || q1.toLowerCase() === 'y'){
     alert('You are look like me');
-}else if('q1 ===no'|| 'q1 === n'){
+}else if(q1.toLowerCase() ==='no'|| q1.toLowerCase() === 'n'){
 alert('You are not like me');
 }else{
-    alert('wellcom'+userName);
+    alert('Nice to meet you'+ userName);
 }
 
-let q1 = prompt('Do you like sports ?');
+
+let q2= prompt('Do you like sports ?');
+console.log(q2);
+if(q2.toLowerCase() === 'yes' || q2.toLowerCase() === 'y'){
+    alert('You are look like me');
+}else if(q2.toLowerCase() ==='no'|| q2.toLowerCase() === 'n'){
+alert('You are not like me');
+}else{
+    alert('Nice to meet you'+ userName);
+}
 
 
-let q1 = prompt('Do you like fighting games?');
+ let q3 = prompt('Do you like fighting games?');
+ console.log(q3);
+ if(q3.toLowerCase() === 'yes' || q3.toLowerCase() === 'y'){
+     alert('You are look like me');
+ }else if(q3.toLowerCase() ==='no'|| q3.toLowerCase() === 'n'){
+ alert('You are not like me');
+ }else{
+     alert('Nice to meet you'+ userName);
+ }
 
+ let q4 = prompt('do you like drawing ?');
+ console.log(q4);
+if(q4.toLowerCase() === 'yes' || q4.toLowerCase() === 'y'){
+    alert('You are look like me');
+}else if(q4.toLowerCase() ==='no'|| q4.toLowerCase() === 'n'){
+alert('You are not like me');
+}else{
+    alert('Nice to meet you'+ userName);
+}
 
-let q1 = prompt('do you like drawing ?');
-
-
-let q1 = prompt('Do you want to be a programmer?');
-
-
+let q5 = prompt('Do you want to be a programmer?');
+console.log(q5);
+if(q5.toLowerCase() === 'yes' || q5.toLowerCase() === 'y'){
+    alert('You are look like me');
+}else if(q5.toLowerCase() ==='no'|| q5.toLowerCase() === 'n'){
+alert('You are not like me');
+}else{
+    alert('Nice to meet you'+ userName);
+}
 
 alert('Nice to meet you'+ userName);
-
-
-
-
-
