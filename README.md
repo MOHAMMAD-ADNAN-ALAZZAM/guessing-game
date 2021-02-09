e635a3d3217e019e859a74310e10cf8e93774a6f
MOHAMMAD ADNAN AL AZZAM <mohammadalazzam730@gmail.com>
2021-02-09 22:03:37 +0200

    red

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # guessing-game
-About Me, with a guessing game
+
+## About Me, with a guessing game
+## Get to know me through a set of questions
+## What are my hobbies
+## What are my interests
--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ if(q1.toLowerCase() === 'yes' || q1.toLowerCase() === 'y'){
 }else if(q1.toLowerCase() ==='no'|| q1.toLowerCase() === 'n'){
 alert('You are not like me');
 }else{
-    alert('Nice to meet you'+ userName);
+    alert('Nice to meet you ' + userName);
 }
 
 
@@ -25,7 +25,7 @@ if(q2.toLowerCase() === 'yes' || q2.toLowerCase() === 'y'){
 }else if(q2.toLowerCase() ==='no'|| q2.toLowerCase() === 'n'){
 alert('You are not like me');
 }else{
-    alert('Nice to meet you'+ userName);
+    alert('Nice to meet you ' + userName);
 }
 
 
@@ -36,7 +36,7 @@ alert('You are not like me');
  }else if(q3.toLowerCase() ==='no'|| q3.toLowerCase() === 'n'){
  alert('You are not like me');
  }else{
-     alert('Nice to meet you'+ userName);
+     alert('Nice to meet you ' + userName);
  }
 
  let q4 = prompt('do you like drawing ?');
@@ -46,7 +46,7 @@ if(q4.toLowerCase() === 'yes' || q4.toLowerCase() === 'y'){
 }else if(q4.toLowerCase() ==='no'|| q4.toLowerCase() === 'n'){
 alert('You are not like me');
 }else{
-    alert('Nice to meet you'+ userName);
+    alert('Nice to meet you ' + userName);
 }
 
 let q5 = prompt('Do you want to be a programmer?');
@@ -56,7 +56,81 @@ if(q5.toLowerCase() === 'yes' || q5.toLowerCase() === 'y'){
 }else if(q5.toLowerCase() ==='no'|| q5.toLowerCase() === 'n'){
 alert('You are not like me');
 }else{
-    alert('Nice to meet you'+ userName);
+    alert('Nice to meet you ' + userName);
 }
-// end alert with username inpot
-alert('Nice to meet you'+ userName);
+
+let myAge = prompt('Guess how old am I?');
+myAge =parseInt(myAge);
+console.log(myAge);
+// while(myAge<30){
+//     console.log(myAge);
+// }
+let m1 = 1;
+while(m1!==4){
+   
+        if(myAge>30){
+
+            alert('TOO HIGH');
+           
+        }else if(myAge<30){
+
+            alert('TOO LOW');
+        }else{
+            
+            
+            alert('correct answer my age = 30');
+            
+            document.write('correct answer my age = 30 you got a point (1)');
+
+             break;
+        }
+       myAge = prompt('Guess how old am I?');
+
+       m1++;
+       
+    
+    }
+    
+    alert('the correct answer is = 30');
+
+
+    
+    let arr =['Pizza','Burger'];
+    let fav = false;
+
+    for (let i = 0; i <6 ; i++){
+        let fav =prompt('Guess my favorite meal?');
+        for (let j = 0; j < arr.length; j++) {
+            if (fav.tolowerCase() === arr[j]){
+                fav = true;
+                alert('correct answer');
+                
+                break;
+                
+            }
+           
+            
+        }
+        if (fav === true){
+            break;
+        }
+
+          fav=prompt('wrong answer! try again ?');  
+    }
+
+    document.write(arr);
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
